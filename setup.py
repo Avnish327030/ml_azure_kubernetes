1412c51ee9983b5eed5954367d7544f62551bf9c
Avnish327030 <anishyadav7045075175@gmail.com>
2021-12-05 11:15:22 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wheat-kernal",
+    version="0.0.3",
+    description="wheat-kernal",
+    author="Avnish yadav", 
+    packages=find_packages(),
+    license="MIT"
+)
